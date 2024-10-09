5ef3ad5f3c986c6f585c49512a754139488db2bf
xzheng <xiqian.zheng@tuodao.com.sg>
2024-10-09 10:35:17 +0800

    incremental update

--- 257 Binary Tree Pass/main.cpp
+++ 257 Binary Tree Pass/main.cpp
@@ -1,8 +1,23 @@
 #include "../pub/pub.hpp"
+#include <string>
 
 
-vector<string> binarytreePaths(TreeNode* root){
-    
+vector<string> binaryTreePaths(TreeNode* root){
+    vector<string> res;
+    if(root == nullptr) return res;
+    if(root->left == nullptr && root->right == nullptr){
+        res.push_back(to_string(root->val));
+        return res;
+    }
+    vector<string> left = binaryTreePaths(root->left);
+    for(auto s : left){
+        res.push_back(to_string(root->val) + "->" + s);
+    }
+    vector<string> right = binaryTreePaths(root->right);
+    for(auto s : right){
+        res.push_back(to_string(root->val) + "->" + s);
+    }
+    return res;
 }
 
 
